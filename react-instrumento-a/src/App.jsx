ca4ad4c8b2529728c761237fc341d615a8b31c6a
Camilo Gomez <elcanserbero450@gmail.com>
2024-06-13 07:58:41 -0500

    Component Error 404

--- react-instrumento-a/src/App.jsx
+++ react-instrumento-a/src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Card } from './Card/Card';
 import { Welcome } from './Welcome/Welcome';
 import { Route, Routes } from 'react-router-dom';
+import { NotFound } from './NotFound/NotFound';
 
 //En este codigo o archivo app.jsx importamos el react router dom para poder llamar las rutas necesarias 
 
@@ -13,7 +14,8 @@ export const App = () => {
     <> 
       <Welcome />   
        <Routes>
-        <Route path='/Card' element= {<Card />}/>        
+        <Route path='/Card' element= {<Card />}/>
+        <Route path='*' element= {<NotFound />}/>        
        </Routes> 
     </> 
   )
